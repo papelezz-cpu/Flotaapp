10627aeddbbf8b1e424df8e12048c0b8cf4f9f95
omarsilv <omar_silvap@hotmail.com>
2026-09-14 17:53:45 -0600

    La app de produccion usa la clave publishable

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const { createClient } = supabase;
 
 const sb = createClient(
   'https://xnyqsewaluezkkrlyhxg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueXFzZXdhbHVlemtrcmx5aHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTMzMTcsImV4cCI6MjA5MTY2OTMxN30.1WSfnJgMM1EPehd3fIMfDTUZqD8hEfhd8xYFCynBMhA',
+  'sb_publishable_Y02FH1PJFo3TsNqS6eLaXQ_URS7LEsk',
   { auth: { storage: window.sessionStorage, persistSession: true } }
 );
 
